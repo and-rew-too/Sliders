6f43fb8c378807e6b99c090139474ae04e53b794
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2021-12-29 00:55:57 -0800

    Create hotspotshading-test.py

---
+++ hotspotshading-test.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd 
+
+
+
+
+q = 1.62*10**-19
+k = 1.38*10**-23
+T = 298
+Io = ?? 
+Rs = ???
+Rsh = 1000 
+
+a = 0.1???
+Vb = -15
+m = -3.7
+
+THAT WILL ESTIMATE IT AT A SET POINT.. I
+THEN NEED TO INCREASE THE I's tested for along zero current (Voc) to Iph ~ 9A, smal lstep sizes too
+#I = linspace(
+#for i in range(1,len(I.index())): 
+#  while ,,
+#    ...
+  
+  
+  
+  
+###########################################################################################3
+
+#f(V) = 0.04*15.6*15.6 ) *shading - Io**((q*V+I*Rs)/(k*T)) - V+(I*Rs)/Rsh * (1+)
+#f'(V) =  ???????????
+
+Vint = 0.5
+while error >= 0.05
+  funcV = f(Vint)
+  funcderivV = f'(Vint)
+  Vnew = Vint - f(Vint)/f'(Vint)
+  error = Vnew-Vint
+  
+  Vint = Vnew #reassign newly calculated value as the new 
+  
+  
+print(Vint)
+  
+
+
