d0847c3715090fbd042ee69454ee791a14ebcfd1
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2021-12-29 23:39:23 -0800

    Update hotspotshading-test.py

--- hotspotshading-test.py
+++ hotspotshading-test.py
@@ -24,20 +24,43 @@ m = 3.7
 #f(V) = -I + (0.04*15.6*15.6) - Io*np.exp((q*Vint+I*Rs)/(k*T)) - ( (Vint+I*Rs)/Rsh )* (1+)
 #f'(V) = (-Io*q)/(k*T) * np.exp((q*Vint+I*Rs)/(k*T)) - (1+(a*(1- V+I*Rs)/Vb)**(-m)
 
-# INCLUDE FOR LOOP OF linspace(0,11, 50 steps) 
+
 # INCLUE A BREAK STATEMENT OFR IF THE VALUES EVER GET TO NEGATIVE (that means way above current 9 or 10, so goes -Inf
 
-I = 5.0
-Vint = 0.7
-error = 10000
-while error >= 0.005:
-  #Vnew = Vint - f(Vint)/f'(Vint)
-  Vnew = Vint - ( -I + (0.044*15.6*15.6) - Io*np.exp((q*(Vint+I*Rs)/(k*T)))  ) /  (  (-Io*q)/(k*T) * np.exp((q*(Vint+I*Rs))/(k*T))  )                                                                       
-  print(Vnew)
-  error = abs(Vnew-Vint)
+# Vint = 0.7
+# error = 10000
+# while error >= 0.005:
+#   #Vnew = Vint - f(Vint)/f'(Vint)
+#   Vnew = Vint - ( -I + (0.044*15.6*15.6) - Io*np.exp((q*(Vint+I*Rs)/(k*T)))  ) /  (  (-Io*q)/(k*T) * np.exp((q*(Vint+I*Rs))/(k*T))  )                                                                       
+#   error = abs(Vnew-Vint)
   
-  Vint = Vnew #reassign newly calculated value as the new 
-  print(Vint)
+#   Vint = Vnew #reassign newly calculated value as the new 
+#   print(Vint)
+Iarray = np.linspace(0,10,num=51)
+Varray = pd.Dataframe(np.zeros([1,51]))
+for i in range(0,51):
+  Vint = 0.7
+  error = 10000
+  while error > 0.005:
+    Vnew = Vint - ( -I + (0.044*15.6*15.6) - Io*np.exp((q*(Vint+I*Rs)/(k*T)))  ) /  (  (-Io*q)/(k*T) * np.exp((q*(Vint+I*Rs))/(k*T))  )    
+    error = abs(Vnew-Vint)
+    Vint = Vnew #reassign newly calculated value as the new 
+  Varray.iloc[0,i] = Vint
+print(Varray)
+  
+  
+# fig = go.Figure(data=go.Scatter(x=I, y=Vint))
+
+# fig.update_layout(yaxis_range=[-0.1, 1.5])
+# fig.update_layout(
+#     title="PV Open Circuit Voltage as a function of Temperature",
+#     font=dict(
+#         family="Courier New, monospace",
+#         size=18,
+#         color="RebeccaPurple"
+#     )
+# )
+
  
   
 
