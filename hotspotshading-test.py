c02f56f435c402f29e44b84d9cb2aff923e9f1ac
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-01-16 00:55:05 -0800

    Update hotspotshading-test.py

--- hotspotshading-test.py
+++ hotspotshading-test.py
@@ -3,6 +3,12 @@ import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
+def find_nearest(array, value):
+    array = np.nan_to_num(array)
+    idx = (np.abs(array - value)).argmin()
+    #return array[idx]
+    return idx
+
 q = 1.602*10**(-19)
 k = 1.3806*10**(-23)
 T = 298
@@ -14,22 +20,14 @@ Ns = 20
 a = 0.1 #IDK change this later
 Vb = -15
 m = 3.7
-# f(V) = -I + (0.04*15.6*15.6) - Io*np.exp((q*Vint+I*Rs)/(k*T))
-# f'(V) = (-Io*q)/(k*T) * np.exp((q*Vint+I*Rs)/(k*T))
-###########################################################################################3
-#f(V) = -I + (0.04*15.6*15.6) - Io*np.exp((q*Vint+I*Rs)/(k*T)) - ( (Vint+I*Rs)/Rsh )* (1+)
-#f'(V) = (-Io*q)/(k*T) * np.exp((q*Vint+I*Rs)/(k*T)) - (1+(a*(1- V+I*Rs)/Vb)**(-m)
-
-
 #imagine a standard 60 cell array with 3 bypass diodes
-# Varray is the shaded array
+# Iarray/Varray is the shaded array
 # V1 is the 'unshaded' first string of 20 cells
-# v2 is the 'unshaded' second string of 20 cells
-# v3 is the 'unshded' third string of 20 cells
-#VALUE IN Iarray determines how far we plot the values, and how far we get V values
-Iarray = np.linspace(200,1,num=401)
-Varray = np.zeros([401])
-for i in range(0,401):
+# V2 is the 'unshaded' second string of 20 cells
+# V3 is the 'unshded' third string of 20 cells
+Iarray = np.linspace(200,1,num=301)
+Varray = np.zeros([301])
+for i in range(0,301):
   Vint = 0.66
   error = 10000
   I = Iarray[i]
@@ -44,8 +42,8 @@ for i in range(0,401):
   Varray[i] = Vint
 
 #finding the index at which all those values are getting skipped through
-Vmax = np.zeros([401])
-for i in range(1,401):
+Vmax = np.zeros([301])
+for i in range(1,301):
     Vmax[i] = Varray[i]-Varray[i-1]
 df = pd.DataFrame(Vmax, columns = ['Vmax'])
 column = df["Vmax"]
@@ -90,6 +88,36 @@ print(V1)
 #         pass
 
 
+
+
+
+from numpy import inf
+Varray = np.nan_to_num(Varray)
+Varray[Varray == 0] = inf
+#V1 already has infinity values?
+
+df = pd.DataFrame(np.zeros([301,200]))
+for i in range(0,301):
+    for j in range(0,200):
+        dist = abs(Varray[i]-V1[j])+abs(Iarray[i]-Iarray1[j])
+        df.iloc[i,j] = dist
+A = df.stack().idxmin()
+#A is a 1x2 tuple containing the row and the column of values
+#HARD CODE, best values are 108 and also 287
+print(type(df.stack().idxmin()))
+
+print(V1[A[1]])
+print(Varray[A[0]])
+Pdisappate = -Varray[287]*Iarray[287]
+#diodePdisappate = Varray[287]*(Isc-Iarray[287])
+print(Pdisappate)
+exit()
+#############################################################################
+#THE ABOVE is the first half, during which the single diode is overlapped with
+#the (-x) V2 array to find the operating point, while down below
+#second half is appending all values into a single IV curve at 2 appropriate points
+
+
 Iarray2 = np.linspace(15,0.1,num=301)
 V2 = np.zeros([301])
 for i in range(0,301):
@@ -117,7 +145,6 @@ for j in range(300,0,-1):
     else:
         pass
 
-
 Iarray3 = np.linspace(15,0.1,num=301)
 V3 = np.zeros([301])
 for i in range(0,301):
@@ -132,8 +159,7 @@ for i in range(0,301):
     if Vnew > 1000000:
         pass
     error = abs(Vnew-Vint)
-    #print(error)
-    Vint = Vnew #reassign newly calculated value as the new
+    Vint = Vnew
     V3[i] = Vint
     V3[i] = -V3[i]
 #WHAT THIS DOES IS at the overflow point, where I increases so dramatically,
@@ -144,28 +170,18 @@ for j in range(300,0,-1):
         Iarray3[j] = Iarray3[j+1]
     else:
         pass
-
-
-
-
 #################################################################################
 # FINDS where the string V3 begins to diverge, that's the Vmp and then
 # splice the strings together into a single large IV curve with summed voltage
 V3 = -V3
 V1 = -V1
 V2 = -V2
-def find_nearest(array, value):
-    array = np.nan_to_num(array)
-    idx = (np.abs(array - value)).argmin()
-    #return array[idx]
-    return idx
 
 # NUMBER AT voltage.. AT WHICH TO SPLICE
+# below is now appending V3 to V1, 2 out of 3 done
 Vsp = 13
 Splice = find_nearest(V1, Vsp)
 print(find_nearest(V1, Vsp))
-
-# below is now appending V3 to V1, 2 out of 3 done
 V3 = V3 + Vsp + 0.7 # THE VALUE of the bypass diode
 V1 = np.append(V1[0:Splice],V3)
 Iarray1 = np.append(Iarray1[0:Splice],Iarray3)
@@ -191,20 +207,18 @@ fig.add_trace(go.Scatter(x=V1, y=Iarray1,
 #                    name='markers'))
 
 fig.add_shape(type='line',
-                x0=50,
-                y0=0,
-                x1=-20,
-                y1=0,
+                x0=50, y0=0, x1=-20, y1=0,
                 line={'dash': 'dash', 'color': 'red'},
                 xref='x',
                 yref='y')
 fig.add_shape(type='line',
-                x0=0,
-                y0=15,
-                x1=0,
-                y1=0,
+                x0=0, y0=15, x1=0, y1=0,
                 line={'dash': 'dash', 'color': 'red'},
                 xref='x',
                 yref='y')
-
 fig.show()
+# f(V) = -I + (0.04*15.6*15.6) - Io*np.exp((q*Vint+I*Rs)/(k*T))
+# f'(V) = (-Io*q)/(k*T) * np.exp((q*Vint+I*Rs)/(k*T))
+###########################################################################################3
+#f(V) = -I + (0.04*15.6*15.6) - Io*np.exp((q*Vint+I*Rs)/(k*T)) - ( (Vint+I*Rs)/Rsh )* (1+)
+#f'(V) = (-Io*q)/(k*T) * np.exp((q*Vint+I*Rs)/(k*T)) - (1+(a*(1- V+I*Rs)/Vb)**(-m)
